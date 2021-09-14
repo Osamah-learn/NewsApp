a3973827f795229b8d87717f34fbb64321cf8308
Osamah Amer <osamah.amer@learnit.fi>
2021-09-14 17:33:21 +0300

    roboto_medium Bug fixed

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import TabScreen from "./src/screens/TabScreen";
+import * as Font from 'expo-font';
+
+
+
+
+
 
 export default function App() {
+
+  React.useEffect(() => {
+    (async () => await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    }))();
+   }, [])
   return <TabScreen />;
 }
